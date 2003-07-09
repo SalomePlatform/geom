d4e00c3e52f19678146906d858a31624633effae
nri <nri@opencascade.com>
2003-07-09 14:50:35 +0000

    NRI : Merge from V1_2.

--- src/GEOMGUI/GeometryGUI_WireDlg.cxx
+++ src/GEOMGUI/GeometryGUI_WireDlg.cxx
@@ -1,12 +1,32 @@
-using namespace std;
-//  File      : GeometryGUI_WireDlg.cxx
-//  Created   :
-//  Author    : Lucien PIGNOLONI
-//  Project   : SALOME
-//  Module    : GeometryGUI
-//  Copyright : OPEN CASCADE
+//  GEOM GEOMGUI : GUI for Geometry component
+//
+//  Copyright (C) 2003  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS 
+// 
+//  This library is free software; you can redistribute it and/or 
+//  modify it under the terms of the GNU Lesser General Public 
+//  License as published by the Free Software Foundation; either 
+//  version 2.1 of the License. 
+// 
+//  This library is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+//  Lesser General Public License for more details. 
+// 
+//  You should have received a copy of the GNU Lesser General Public 
+//  License along with this library; if not, write to the Free Software 
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA 
+// 
+//  See http://www.opencascade.org/SALOME/ or email : webmaster.salome@opencascade.org 
+//
+//
+//
+//  File   : GeometryGUI_WireDlg.cxx
+//  Author : Lucien PIGNOLONI
+//  Module : GEOM
 //  $Header$
 
+using namespace std;
 #include "GeometryGUI_WireDlg.h"
 
 #include "GeometryGUI.h"
