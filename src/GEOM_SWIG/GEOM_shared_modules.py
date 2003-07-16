ee49307ec13fbd93f8c81968706c68044f0a10a1
nri <nri@opencascade.com>
2003-07-16 07:57:24 +0000

    NRI : Force import GEOM.

---
+++ src/GEOM_SWIG/GEOM_shared_modules.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+# force GEOM importation at interpretor initialization
+# see salome_shared_modules.py
+# (avoids incomplete import at run time)
+
+print "============== import GEOM ======================="
+
+import GEOM
+
+# this function is required
+
+def init_shared_modules():
+   """
+      This function initializes shared modules that need to be
+   """
+   pass
