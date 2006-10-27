a4f61153508384351bfad800d976bb7f016aa669
epa <epa@opencascade.com>
2006-10-27 08:52:22 +0000

    PAL 11107 - Make a rotation given an object and 3 points

---
+++ src/DlgRef/DlgRef_4Sel1Spin2Check_QTD.h
@@ -0,0 +1,63 @@
+/****************************************************************************
+** Form interface generated from reading ui file 'DlgRef_4Sel1Spin2Check_QTD.ui'
+**
+** Created: Wed Oct 25 20:32:41 2006
+**      by: The User Interface Compiler ($Id$)
+**
+** WARNING! All changes made in this file will be lost!
+****************************************************************************/
+
+#ifndef DLGREF_4SEL1SPIN2CHECK_QTD_H
+#define DLGREF_4SEL1SPIN2CHECK_QTD_H
+
+#include <qvariant.h>
+#include <qwidget.h>
+
+class QVBoxLayout;
+class QHBoxLayout;
+class QGridLayout;
+class QSpacerItem;
+class QGroupBox;
+class QCheckBox;
+class QPushButton;
+class QLineEdit;
+class QLabel;
+class QSpinBox;
+
+class DlgRef_4Sel1Spin2Check_QTD : public QWidget
+{
+    Q_OBJECT
+
+public:
+    DlgRef_4Sel1Spin2Check_QTD( QWidget* parent = 0, const char* name = 0, WFlags fl = 0 );
+    ~DlgRef_4Sel1Spin2Check_QTD();
+
+    QGroupBox* GroupBox1;
+    QCheckBox* CheckButton2;
+    QPushButton* PushButton2;
+    QLineEdit* LineEdit5;
+    QLabel* TextLabel5;
+    QLabel* TextLabel1;
+    QLineEdit* LineEdit1;
+    QSpinBox* SpinBox1;
+    QCheckBox* CheckButton1;
+    QLabel* TextLabel2;
+    QPushButton* PushButton5;
+    QPushButton* PushButton4;
+    QLabel* TextLabel4;
+    QPushButton* PushButton1;
+    QLineEdit* LineEdit4;
+    QLineEdit* LineEdit2;
+    QLabel* TextLabel3;
+
+protected:
+    QVBoxLayout* DlgRef_4Sel1Spin2Check_QTDLayout;
+    QVBoxLayout* GroupBox1Layout;
+    QGridLayout* Layout1;
+
+protected slots:
+    virtual void languageChange();
+
+};
+
+#endif // DLGREF_4SEL1SPIN2CHECK_QTD_H
