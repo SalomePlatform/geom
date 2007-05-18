645aebdeb7ee9d7c4679e7dfc1031830b09ec1fe
eap <eap@opencascade.com>
2007-05-18 13:47:36 +0000

    NPAL15983 (EDF PAL 413: Bug when displaying empty groups)    /* Case of only one subshape */ -  if (list->length() == 1) { +  if (list->length() == 1 && list[0] > 0) {      S = anIndices.FindKey(list[0]);    }

--- src/GEOMClient/GEOM_Client.cxx
+++ src/GEOMClient/GEOM_Client.cxx
@@ -222,7 +222,7 @@ TopoDS_Shape GEOM_Client::GetShape( GEOM::GEOM_Gen_ptr geom, GEOM::GEOM_Object_p
   TopExp::MapShapes(aMainShape, anIndices);
 
   /* Case of only one subshape */
-  if (list->length() == 1) {
+  if (list->length() == 1 && list[0] > 0) {
     S = anIndices.FindKey(list[0]);
   }
   else {
