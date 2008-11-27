b7d82b4f7b59952a6f75f359b8cccd5d7ec075a3
abd <abd@opencascade.com>
2008-11-27 11:37:49 +0000

    Update copyright information

--- src/RepairGUI/RepairGUI_RemoveExtraEdgesDlg.cxx
+++ src/RepairGUI/RepairGUI_RemoveExtraEdgesDlg.cxx
@@ -1,6 +1,6 @@
-//  GEOM RepairGUI : GUI for Geometry component
+//  Copyright (C) 2007-2008  CEA/DEN, EDF R&D, OPEN CASCADE
 //
-//  Copyright (C) 2003  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
 //  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
 //
 //  This library is free software; you can redistribute it and/or
@@ -10,7 +10,7 @@
 //
 //  This library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //  Lesser General Public License for more details.
 //
 //  You should have received a copy of the GNU Lesser General Public
@@ -19,13 +19,12 @@
 //
 //  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
-//
-//
+//  GEOM RepairGUI : GUI for Geometry component
 //  File   : RepairGUI_RemoveExtraEdgesDlg.cxx
 //  Author : Michael Zorin
 //  Module : GEOM
 //  $Header$
-
+//
 #include "RepairGUI_RemoveExtraEdgesDlg.h"
 
 #include "SalomeApp_Application.h"
