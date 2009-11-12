e39164dd0ecc56a720944563e2bf2afaecefb9c7
vsr <vsr@opencascade.com>
2009-11-12 13:38:56 +0000

    TC5.1.3: a shape is shown with edges direction by default in VTK, but shouldn't be (additional minor fix)

--- src/GEOMGUI/GEOM_Displayer.cxx
+++ src/GEOMGUI/GEOM_Displayer.cxx
@@ -849,9 +849,7 @@ void GEOM_Displayer::Update( SALOME_VTKPrs* prs )
   }
   else
   {
-    bool isVector = false;
-    if (myType == GEOM_VECTOR) isVector = true;
-    theActors = GEOM_AssemblyBuilder::BuildActors( myShape, 0, 0, Standard_True, isVector );
+    theActors = GEOM_AssemblyBuilder::BuildActors( myShape, 0, 0, Standard_True, myType == GEOM_VECTOR );
   }
 
   theActors->InitTraversal();
