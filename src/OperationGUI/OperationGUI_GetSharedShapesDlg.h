d93b998d309a0f6d4455316be5c3fa90c8a51874
jfa <jfa@opencascade.com>
2010-12-16 08:15:45 +0000

    Mantis issue 0020939: To have in the GUI the function GetSharedShapes().

--- src/OperationGUI/OperationGUI_GetSharedShapesDlg.h
+++ src/OperationGUI/OperationGUI_GetSharedShapesDlg.h
@@ -48,7 +48,7 @@ protected:
   virtual bool                       isValid (QString&);
   virtual bool                       execute (ObjectList&);
   virtual GEOM::GEOM_Object_ptr      getFather (GEOM::GEOM_Object_ptr);
-  virtual QString                    getNewObjectName() const;
+  QString                            getPrefixByType() const;
 
 private:
   void                               Init();
