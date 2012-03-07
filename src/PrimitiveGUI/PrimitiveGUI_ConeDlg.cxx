aa5b2185c94e6ddb70859f678478183379b8a84f
ana <ana@opencascade.com>
2012-03-07 08:45:19 +0000

    Fix for the bug IPAL22807:"TC6.4.0: input objects are not highlighted in 3D viewer"

--- src/PrimitiveGUI/PrimitiveGUI_ConeDlg.cxx
+++ src/PrimitiveGUI/PrimitiveGUI_ConeDlg.cxx
@@ -268,22 +268,24 @@ void PrimitiveGUI_ConeDlg::SelectionIntoArgument()
     QString aName = GEOMBase::GetName( aSelectedObject.get() );
 
     myEditCurrentArgument->setText(aName);
-
-    // clear selection
-    disconnect(myGeomGUI->getApp()->selectionMgr(), 0, this, 0);
-    myGeomGUI->getApp()->selectionMgr()->clearSelected();
-    connect(myGeomGUI->getApp()->selectionMgr(), SIGNAL(currentSelectionChanged()),
-	    this, SLOT(SelectionIntoArgument()));
     
     if (myEditCurrentArgument == GroupPoints->LineEdit1) {
       myPoint = aSelectedObject;
       if (myPoint && !myDir)
-	GroupPoints->PushButton2->click();
+        GroupPoints->PushButton2->click();
     }
     else if (myEditCurrentArgument == GroupPoints->LineEdit2) {
       myDir = aSelectedObject;
       if (myDir && !myPoint)
-	GroupPoints->PushButton1->click();
+        GroupPoints->PushButton1->click();
+    }
+
+    if ((myPoint && !myDir) || (myDir && !myPoint)) {
+      // clear selection
+      disconnect(myGeomGUI->getApp()->selectionMgr(), 0, this, 0);
+      myGeomGUI->getApp()->selectionMgr()->clearSelected();
+      connect(myGeomGUI->getApp()->selectionMgr(), SIGNAL(currentSelectionChanged()),
+	      this, SLOT(SelectionIntoArgument()));
     }
   }
 
