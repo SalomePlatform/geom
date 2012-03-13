f33f73b43533ad803aa7646f176395796a5b4c05
rnv <rnv@opencascade.com>
2012-03-13 08:42:02 +0000

    Small correction.

--- src/GEOM/GEOM_Function.cxx
+++ src/GEOM/GEOM_Function.cxx
@@ -209,7 +209,7 @@ TopoDS_Shape GEOM_Function::GetValue()
 
     if (!isResult) {
       try {
-#if OCC_VERSION_LARGE > 0x060100
+#if OCC_VERSION_LARGE > 0x06010000
         OCC_CATCH_SIGNALS;
 #endif
         GEOM_Solver aSolver(GEOM_Engine::GetEngine());
