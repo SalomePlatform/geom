0bbd959702455434a5f0eba575d1c2a8b4801b84
eap <eap@opencascade.com>
2012-06-20 09:50:12 +0000

    0021672: [CEA 565] Dump Study from script
       GetSubShapeIndices() from the last function

--- src/GEOM_I/GEOM_Object_i.cc
+++ src/GEOM_I/GEOM_Object_i.cc
@@ -453,7 +453,7 @@ GEOM::ListOfLong* GEOM_Object_i::GetSubShapeIndices()
   GEOM::ListOfLong_var anIndices = new GEOM::ListOfLong;
 
   if(!_impl->IsMainShape()) {
-    Handle(GEOM_Function) aFunction = _impl->GetFunction(1); //Get Sub-shape function (always the first (and last)  one)
+    Handle(GEOM_Function) aFunction = _impl->GetLastFunction(); //Get Sub-shape function (always the first (and last)  one)
     if(aFunction.IsNull()) return anIndices._retn();
     GEOM_ISubShape ISS(aFunction);
     Handle(TColStd_HArray1OfInteger) anArray = ISS.GetIndices();
