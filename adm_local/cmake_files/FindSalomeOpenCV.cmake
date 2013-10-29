b998652209b3fc710f9fcf89ab4c88ec7c082f78
mpa <mpa@opencascade.com>
2013-10-29 08:07:30 +0000

    CMake: - Accumulate environment variables, to be used later to run some command in proper environment.

--- adm_local/cmake_files/FindSalomeOpenCV.cmake
+++ adm_local/cmake_files/FindSalomeOpenCV.cmake
@@ -19,9 +19,13 @@
 # Author: Adrien Bruneton
 #
 
-# OpenCV detection dor Salome
+# OpenCV detection for Salome
 #
 #  !! Please read the generic detection procedure in SalomeMacros.cmake !!
 #
 SALOME_FIND_PACKAGE_AND_DETECT_CONFLICTS(OpenCV OpenCV_DIR 2)
 #MARK_AS_ADVANCED()
+
+IF(OpenCV_FOUND) 
+  SALOME_ACCUMULATE_ENVIRONMENT(LD_LIBRARY_PATH ${OpenCV_LIBRARIES})
+ENDIF()
