ff7fc4d6c2f5e6de741fd7f0b9ebf01de04d0e10
akl <alexander.kovalev@opencascade.com>
2015-02-10 13:25:19 +0400

    Copyrights update 2015.

--- src/GEOM_SWIG/GEOM_TestField.py
+++ src/GEOM_SWIG/GEOM_TestField.py
@@ -1,23 +1,23 @@
- # Copyright (C) 2013-2014  CEA/DEN, EDF R&D, OPEN CASCADE
- #
- # This library is free software; you can redistribute it and/or
- # modify it under the terms of the GNU Lesser General Public
- # License as published by the Free Software Foundation; either
- # version 2.1 of the License, or (at your option) any later version.
- #
- # This library is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- # Lesser General Public License for more details.
- #
- # You should have received a copy of the GNU Lesser General Public
- # License along with this library; if not, write to the Free Software
- # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- #
- # See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
- #
- 
- # Python API for field on geometry manipulations:
+# Copyright (C) 2013-2015  CEA/DEN, EDF R&D, OPEN CASCADE
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+
+# Python API for field on geometry manipulations:
 
 # field          = geompy.CreateField(shape, name, type, dimension, componentNames)
 # geompy.RemoveField(field)
