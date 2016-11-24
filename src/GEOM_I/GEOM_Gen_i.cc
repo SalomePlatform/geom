b8c725d044864576dfeb87ff6200aa745da735a6
skv <skv@opencascade.com>
2016-11-24 13:50:48 +0300

    Fix pb with save/restore GEOM for Debian 6.0

--- src/GEOM_I/GEOM_Gen_i.cc
+++ src/GEOM_I/GEOM_Gen_i.cc
@@ -84,7 +84,7 @@
  #define UnLoadLib( handle ) FreeLibrary( handle );
 #else
  #define LibHandle void*
- #define LoadLib( name ) dlopen( name, RTLD_LAZY )
+ #define LoadLib( name ) dlopen( name, RTLD_LAZY | RTLD_GLOBAL )
  #define GetProc dlsym
  #define UnLoadLib( handle ) dlclose( handle );
 #endif
