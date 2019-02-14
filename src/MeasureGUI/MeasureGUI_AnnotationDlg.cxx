d6f1d8730fa07983755573173bfdf8f708f91a17
vsr <vsr@opencascade.com>
2019-02-14 13:09:13 +0300

    Update copyrights

--- src/MeasureGUI/MeasureGUI_AnnotationDlg.cxx
+++ src/MeasureGUI/MeasureGUI_AnnotationDlg.cxx
@@ -1,12 +1,9 @@
-// Copyright ( C ) 2007-2016  CEA/DEN, EDF R&D, OPEN CASCADE
-//
-// Copyright ( C ) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
-// CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+// Copyright (C) 2016-2019  CEA/DEN, EDF R&D, OPEN CASCADE
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
-// version 2.1 of the License, or ( at your option ) any later version.
+// version 2.1 of the License, or (at your option) any later version.
 //
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
