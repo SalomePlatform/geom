3bfb88115e12b64d01e3e5606e4b84184deaceb2
NATHALIE GORE <ng13417n@dsp1077169.postes.calibre.edf.fr>
2024-02-12 15:13:33 +0100

    Revert "EDF 25230 - Impossible to create directly a face"
    This reverts commit aecc16bb8400d9673445a0485fc625ed28f71b9c.

--- src/GEOMImpl/GEOMImpl_ShapeDriver.cxx
+++ src/GEOMImpl/GEOMImpl_ShapeDriver.cxx
@@ -494,7 +494,6 @@ Standard_Integer GEOMImpl_ShapeDriver::Execute(Handle(TFunction_Logbook)& log) c
     Handle(TopTools_HSequenceOfShape) aSeqEdgesIn = new TopTools_HSequenceOfShape;
     TColStd_IndexedDataMapOfTransientTransient aMapTShapes;
 
-    Standard_Real aTolerance = Precision::Confusion();
     for (ind = 1; ind <= nbshapes; ind++) {
       Handle(GEOM_Function) aRefSh_i = Handle(GEOM_Function)::DownCast(aShapes->Value(ind));
       TopoDS_Shape aSh_i = aRefSh_i->GetValue();
@@ -504,11 +503,7 @@ Standard_Integer GEOMImpl_ShapeDriver::Execute(Handle(TFunction_Logbook)& log) c
         if (aMapEdges.Add(anExpE_i.Current())) {
           // Copy the original shape.
           TopoDS_Shape aShapeCopy;
-          Standard_Real aCurTol = BRep_Tool::MaxTolerance(anExpE_i.Current(), TopAbs_EDGE);
-          if(aCurTol > aTolerance)
-          {
-            aTolerance = aCurTol;
-          }
+
           TNaming_CopyShape::CopyTool
             (anExpE_i.Current(), aMapTShapes, aShapeCopy);
           aSeqEdgesIn->Append(aShapeCopy);
@@ -522,7 +517,7 @@ Standard_Integer GEOMImpl_ShapeDriver::Execute(Handle(TFunction_Logbook)& log) c
 
     // 2. Connect edges to wires of maximum length
     Handle(TopTools_HSequenceOfShape) aSeqWiresOut;
-    ShapeAnalysis_FreeBounds::ConnectEdgesToWires(aSeqEdgesIn, aTolerance,
+    ShapeAnalysis_FreeBounds::ConnectEdgesToWires(aSeqEdgesIn, Precision::Confusion(),
                                                   /*shared*/Standard_False, aSeqWiresOut);
     //modified by NIZNHY-PKV Wed Dec 28 13:46:55 2011f
     //KeepEdgesOrder(aSeqEdgesIn, aSeqWiresOut);
